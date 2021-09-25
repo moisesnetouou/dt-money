ed616d93218ba0ee24257db0dc69b0c239ff838a
Moisés Neto <moisesnetored@gmail.com>
2021-09-25 12:01:57 -0400

    feat: Modal styling, forms and beginning of the use of Context API.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,34 @@
+import { useState } from "react";
+import Modal from "react-modal";
+
 import { Header } from "./components/Header";
 import { Dashboard } from "./components/Dashboard";
+import { NewTransactionModal } from "./components/NewTransactionModal";
+import { TransactionsContext } from "./TransactionsContext";
 
 import { GlobalStyle } from "./styles/global";
 
+Modal.setAppElement("#root");
+
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModal] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModal(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModal(false);
+  }
   return (
-    <>
-      <Header />
+    <TransactionsContext.Provider value={[]}>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
       <GlobalStyle />
-    </>
+    </TransactionsContext.Provider>
   );
 }
