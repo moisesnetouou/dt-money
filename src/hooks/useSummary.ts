51a664ae6f59c084633ca39ee3f6dc7ee4489039
Moisés <moisesnetored@gmail.com>
2023-06-13 14:37:51 -0400

    feat: applying useMemo

--- src/hooks/useSummary.ts
+++ src/hooks/useSummary.ts
@@ -1,4 +1,4 @@
-import { useContext } from 'react'
+import { useMemo } from 'react'
 import { TransactionsContext } from '../contexts/TransactionsContext'
 import { useContextSelector } from 'use-context-selector'
 
@@ -8,24 +8,26 @@ export function useSummary() {
   })
 
   // { income: 0, outcome: 0, total: 0 }
-  const summary = transactions.reduce(
-    (acc, transaction) => {
-      if (transaction.type === 'income') {
-        acc.income += transaction.price
-        acc.total += transaction.price
-      } else {
-        acc.outcome += transaction.price
-        acc.total -= transaction.price
-      }
+  const summary = useMemo(() => {
+    return transactions.reduce(
+      (acc, transaction) => {
+        if (transaction.type === 'income') {
+          acc.income += transaction.price
+          acc.total += transaction.price
+        } else {
+          acc.outcome += transaction.price
+          acc.total -= transaction.price
+        }
 
-      return acc
-    },
-    {
-      income: 0,
-      outcome: 0,
-      total: 0,
-    },
-  )
+        return acc
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    )
+  }, [transactions])
 
   return summary
 }
